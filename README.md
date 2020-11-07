03d42c26de69e9f79132d34ca13e032366391919
jeonwonje <jeonwonje04@gmail.com>
2020-11-08 03:01:28 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# btc-alert
+
+To celebrate bitcoin hitting $15000USD, I created this silly app as an alert inspired from the show 'Silicon Valley' by HBO, Season 5 Episode 3.
+
+_Whenever the price of bitcoin dips below a certain value, it's no longer efficient to mine._
+
---
+++ main.js
@@ -0,0 +1,30 @@
+let priceBTC = [15000, 15000]; // Whenever BTC dips below 15000 (sad)
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+	if (this.readyState == 4 && this.status == 200) {
+		priceBTC.push(JSON.parse(this.responseText).bpi.USD.rate_float);
+		console.log(priceBTC);
+		priceBTC.shift();
+		if (priceBTC[1] < priceBTC[0]) {
+			playSong()
+		}
+
+	}
+};
+
+function getRequest() {
+	xhttp.open("GET", "https://api.coindesk.com/v1/bpi/currentprice/USD.json", true);
+	xhttp.send();
+}
+
+setInterval(getRequest, 2000);
+
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+let context = new AudioContext(); // Init WebAudio context
+// get the audio element
+
+let audio = document.getElementById("audio");
+function playSong() {
+    audio.play();
+}
